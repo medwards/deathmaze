c22f832216e2b7e087fcc7a3aa86cbe8dd911349
Michael Edwards <medwards@walledcity.ca>
2013-07-27 11:59:43 +0200

    DungeonMap will not add non-adjacent Room

--- src/main/java/org/p_one/deathmaze/DungeonMap.java
+++ src/main/java/org/p_one/deathmaze/DungeonMap.java
@@ -10,10 +10,21 @@ public class DungeonMap {
 	}
 
 	public void add(Room newRoom) {
+		boolean adjacentToSomething = false;
+		if(this.rooms.size() == 0) { adjacentToSomething = true; }
 		for(Room room : this.rooms) {
 			if(room.x == newRoom.x && room.y == newRoom.y) {
 				return;
 			}
+			int xDiff = Math.abs(room.x - newRoom.x);
+			int yDiff = Math.abs(room.y - newRoom.y);
+			int totalDiff = xDiff + yDiff;
+			if(totalDiff == 1) {
+				adjacentToSomething = true;
+			}
+		}
+		if(!adjacentToSomething) {
+			return;
 		}
 		this.rooms.add(newRoom);
 	}
--- src/test/java/org/p_one/deathmaze/DungeonMapTest.java
+++ src/test/java/org/p_one/deathmaze/DungeonMapTest.java
@@ -26,4 +26,18 @@ public class DungeonMapTest extends TestCase {
 		map.add(room2);
 		assertEquals(1, map.rooms.size());
 	}
+
+	public void testAddedRoomsMustBeAdjacent() {
+		DungeonMap map = new DungeonMap();
+		Room room = new Room(0, 0, true, true, true, true);
+		map.add(room);
+
+		Room adjacent_room = new Room(0, 1, true, true, true, true);
+		Room not_adj_room = new Room(0, 6, true, true, true, true);
+		map.add(adjacent_room);
+		map.add(not_adj_room);
+
+		assertTrue(map.rooms.contains(adjacent_room));
+		assertFalse(map.rooms.contains(not_adj_room));
+	}
 }
