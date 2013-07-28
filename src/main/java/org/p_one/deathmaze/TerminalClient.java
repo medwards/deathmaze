9c896a46eecdcfa7c4e70e15d7affe68b1b24753
Michael Edwards <medwards@walledcity.ca>
2013-07-28 23:30:33 +0200

    Remove demo rooms

--- src/main/java/org/p_one/deathmaze/TerminalClient.java
+++ src/main/java/org/p_one/deathmaze/TerminalClient.java
@@ -24,16 +24,6 @@ public class TerminalClient {
 		this.gameState = new Game();
 		Room aRoom = new Room(0, 0, true, true, true, true);
 		this.gameState.map.add(aRoom);
-		this.gameState.map.add(new Room(0, -1, false, false, true, false));
-		this.gameState.map.add(new Room(1, 0, false, true, false, true));
-		this.gameState.map.add(new Room(0, 1, true, false, true, false));
-		this.gameState.map.add(new Room(0, 2, true, false, true, false));
-		this.gameState.map.add(new Room(0, 3, true, false, true, false));
-		this.gameState.map.add(new Room(0, 4, true, false, true, false));
-		this.gameState.map.add(new Room(0, 5, true, false, true, false));
-		this.gameState.map.add(new Room(0, 6, true, false, true, false));
-		this.gameState.map.add(new Room(0, 7, true, false, true, false));
-		this.gameState.map.add(new Room(0, 8, true, false, true, false));
 
 		this.screen = TerminalFacade.createScreen();
 		this.writer = new ScreenWriter(screen);
