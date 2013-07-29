661a4aca1ae9aad302a8cfbb7e110b58c5d30402
Michael Edwards <medwards@walledcity.ca>
2013-07-29 22:56:20 +0200

    Remove dead Exit.getRandom code

--- src/main/java/org/p_one/deathmaze/Chit.java
+++ src/main/java/org/p_one/deathmaze/Chit.java
@@ -46,18 +46,5 @@ public enum Chit {
 		NONE,
 		DOOR,
 		CORRIDOR;
-
-		public static Long seed = null;
-
-		public static Exit getRandom() {
-			Random generator;
-			if(Exit.seed != null) {
-				generator = new Random(Exit.seed.longValue());
-			} else {
-				generator = new Random();
-			}
-			Exit.seed = new Long(generator.nextLong());
-			return Exit.values()[generator.nextInt(Exit.values().length)];
-		}
 	}
 }
