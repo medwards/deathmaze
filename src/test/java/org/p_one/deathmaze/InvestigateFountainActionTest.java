0fe5e810fcfe118e7065d4cabe69d1ba3655207e
Michael Edwards <medwards@walledcity.ca>
2013-08-01 17:12:55 +0200

    Forgotten InvestigateFountain test

--- src/test/java/org/p_one/deathmaze/InvestigateFountainActionTest.java
+++ src/test/java/org/p_one/deathmaze/InvestigateFountainActionTest.java
@@ -13,7 +13,21 @@ public class InvestigateFountainActionTest extends TestCase {
 
 		boolean result = action.execute(game);
 
+		assertEquals(true, result);
 		assertEquals(Chit.Feature.NONE, room.getFeature());
 		assertEquals(Game.State.DEAD, game.state);
+	}
+
+	public void testNoActionIfNotFountain() {
+		Game game = new Game(new Long(0));
+		Room room = new Room(0, 0, Chit.DEAD_END_STATUE);
+		game.map.add(room);
+		Action action = new InvestigateFountainAction();
+
+		boolean result = action.execute(game);
+
+		assertEquals(false, result);
+		assertEquals(Chit.Feature.STATUE, room.getFeature());
+		assertEquals(Game.State.PLAYING, game.state);
 	}
 }
